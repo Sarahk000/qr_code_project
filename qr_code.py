61a37a062638cff73d44f052a88c9cc57c2d24b5
Sarah <sarahkhan05.pk@gmail.com>
2025-12-25 17:47:09 -0500

    updated QR code color

--- qr_code.py
+++ qr_code.py
@@ -6,5 +6,5 @@ qr = qrcode.QRCode(version = 1, box_size = 5, border = 5)
 qr.add_data(website_link)
 qr.make()
 
-img = qr.make_image(fill_color = 'black', back_color = 'white')
+img = qr.make_image(fill_color = 'magenta', back_color = 'white')
 img.save('youtube_qr.png')
